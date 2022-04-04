0929b5e5cd11c004974df5f1fbca8231471004a8
y.sekine <yoshitsugu.sekine@offisis.co.jp>
2022-04-04 02:10:27 +0000

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "posts#index"
+
+  devise_for :users
+  devise_scope :user do
+    get "/users/sign_out" => "devise/sessions#destory"
+  end
 end
