0c608c6b90e2f81e3378e2b6da1c9745527c1e28
yoshitsugu.sekine@offisis.co.jp <yoshitsugu.sekine@offisis.co.jp>
2022-04-20 10:02:44 +0900

    Pass a test!

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,31 @@
 require 'rails_helper'
 
+# https://qiita.com/motty93/items/5cbc46ce79a502fcaa65
+# CarrierWaveを使ったときのテスト
+
 RSpec.describe Post, type: :model do
-  it "checks that a comment is mandetory" do
-    user = User.new(name: "test", email: "a@example.com", password: "123456", password_confirmation: "123456")
-    post = user.posts.build(caption: "cap", image: "1.png")
+  let(:image_path) { File.join(Rails.root, 'spec/fixtures/sample.png') }
+  let(:image) { Rack::Test::UploadedFile.new(image_path) }
+
+  before do
+    @user = User.create(name: 'test', email: 'a@example.com', password: '123456', password_confirmation: '123456')
+  end
+
+  it 'checks that a comment is mandetory' do
+    post = @user.posts.build(caption: 'cap', image: image)
     expect(post).to be_valid
+
+    post = @user.posts.build(caption: '', image: image)
+    post.valid?
+    expect(post.errors[:caption]).to include("can't be blank")
+  end
+
+  it 'checks a caption length' do
+    post = @user.posts.build(caption: (1..255).map{'a'}.join, image: image)
+    expect(post).to be_valid
+
+    post = @user.posts.build(caption: (1..256).map{'a'}.join, image: image)
+    post.valid?
+    expect(post.errors[:caption]).to include('is too long (maximum is 255 characters)')
   end
 end
